0057de433969db6891beb935e0f3702591d4455b
Yash <yash@Yashs-MacBook-Air-2.local>
2025-01-10 21:08:16 +0530

    added jwt middleware

---
+++ middlewares/verify-jwt-middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {ACCESS_SECRET} = process.env
+
+const verifyJWT = (req,res,next) => {
+    const authHeader = req.headers["authorization"]
+
+    if(!authHeader) {
+        return res.status(400).json({error: "BAD_REQUEST", description: "Authorization header missing."})
+    }
+    const JWT = authHeader.split(" ")[1]
+    if (!JWT) {
+        return res.status(400).json({error: "BAD_REQUEST", description: "JWT missing."})
+    }
+
+    try {
+        jwt.verify(JWT, ACCESS_SECRET)
+        return res.status(200).json({message: "Login Successful."})
+    } catch (err) {
+        console.error(err)
+        return res.status(401).json({ error: "UNAUTHORIZED", description: "JWT malformed." })
+    }
+}
+
+export default verifyJWT
